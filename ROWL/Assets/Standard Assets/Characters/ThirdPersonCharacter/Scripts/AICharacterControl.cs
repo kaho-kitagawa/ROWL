76aa5229841bf6f4f56043926124e4ce3162015a
kaho-kitagawa <ho.ka1.26.kita@gmail.com>
2021-08-08 13:20:46 +0900

    サウンドを追加した！先週の分もやった！

--- ROWL/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
+++ ROWL/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -9,7 +9,9 @@ namespace UnityStandardAssets.Characters.ThirdPerson
     {
         public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
         public ThirdPersonCharacter character { get; private set; } // the character we are controlling
-        public Transform target;                                    // target to aim for
+        public Transform target;    // target to aim for
+
+        public bool Stop = false;
 
 
         private void Start()
@@ -27,6 +29,12 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         {
             if (target != null)
                 agent.SetDestination(target.position);
+
+            if(Stop)
+            {
+                character.Move(Vector3.zero, false, false);
+                return;
+            }
 
             if (agent.remainingDistance > agent.stoppingDistance)
                 character.Move(agent.desiredVelocity, false, false);
